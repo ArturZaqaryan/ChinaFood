e3f8c6447cadea8510863f9b24a7b30876afa289
EmilMkrtumyan <emilmkrtumyan65@gmail.com>
2024-09-22 22:22:22 +0400

    Improving DataManager and IDishesRepository

--- ChinaFood/Domain/Repositories/Abstract/IDishesRepository.cs
+++ ChinaFood/Domain/Repositories/Abstract/IDishesRepository.cs
@@ -1,14 +1,16 @@
 using ChinaFood.Domain.Entities;
+using ChinaFood.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-
 namespace HotBooking.Domain.Repositories.Abstract
 {
     public interface IDishesRepository
     {
-
+        Dish GetDishByID(Guid id);
+        void AddDish(Dish dish);
+        void RemoveDish(Guid id);
     }
 }
